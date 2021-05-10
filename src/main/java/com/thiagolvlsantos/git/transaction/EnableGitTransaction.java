f77111ea19afe353a806c3e676cc290f881ad5aa
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-05-10 00:58:17 -0300

    .

--- src/main/java/com/thiagolvlsantos/git/transaction/EnableGitTransaction.java
+++ src/main/java/com/thiagolvlsantos/git/transaction/EnableGitTransaction.java
@@ -1,6 +1,8 @@
 package com.thiagolvlsantos.git.transaction;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -10,5 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.thiagolvlsantos.git.transactions")
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface EnableGitTransaction {
 }
