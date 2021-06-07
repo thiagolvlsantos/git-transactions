5b8cb2c2e26bfd997de6ee782551b079b25e587c
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-06-07 20:59:51 -0300

    Reduce complexity

--- src/main/java/io/github/thiagolvlsantos/git/transactions/file/FileListenerGit.java
+++ src/main/java/io/github/thiagolvlsantos/git/transactions/file/FileListenerGit.java
@@ -4,6 +4,7 @@ import org.eclipse.jgit.api.AddCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.RmCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
 import org.eclipse.jgit.dircache.DirCache;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,47 +32,66 @@ public class FileListenerGit implements ApplicationListener<FileEvent> {
 				String pattern = provider.normalizeWrite(group, item.getFile().toString());
 				switch (item.getStatus()) {
 				case CREATE:
-					AddCommand add = git.add().addFilepattern(pattern);
-					msg.append("Added: " + pattern + "\n");
-					DirCache addResult = add.call();
-					if (log.isDebugEnabled()) {
-						log.debug(group + ".ADDED: " + addResult);
-					}
+					create(group, git, msg, pattern);
 					break;
 				case MODIFY:
-					AddCommand update = git.add().addFilepattern(pattern);
-					msg.append("Updated: " + pattern + "\n");
-					DirCache addResultUpdate = update.call();
-					if (log.isDebugEnabled()) {
-						log.debug(group + ".UPDATED: " + addResultUpdate);
-					}
+					modify(group, git, msg, pattern);
 					break;
 				case DELETE:
-					RmCommand delete = git.rm().addFilepattern(pattern);
-					msg.append("Deleted: " + pattern + "\n");
-					DirCache deleteResult = delete.call();
-					if (log.isDebugEnabled()) {
-						log.debug(group + ".DELETED: " + deleteResult);
-					}
+					delete(group, git, msg, pattern);
 					break;
 				}
 			}
 			if (msg.length() > 0) {
 				msg.setLength(msg.length() - 1);
-				String tmp = msg.toString();
-				if (log.isInfoEnabled()) {
-					log.info(group + ".MESSAGE:\n" + tmp);
-				}
-				RevCommit commit = provider.commitWrite(group, tmp);
-				if (log.isInfoEnabled()) {
-					log.info(group + ".COMMIT: " + commit);
-				}
+				commitWrite(group, msg, provider);
 			}
 		} catch (GitAPIException e) {
-			if(log.isDebugEnabled()) {
-				log.debug(e.getMessage(),e);
+			if (log.isDebugEnabled()) {
+				log.debug(e.getMessage(), e);
 			}
 			throw new RuntimeException(e);
+		}
+	}
+
+	private void create(String group, Git git, StringBuilder msg, String pattern)
+			throws GitAPIException, NoFilepatternException {
+		AddCommand add = git.add().addFilepattern(pattern);
+		msg.append("Added: " + pattern + "\n");
+		DirCache addResult = add.call();
+		if (log.isDebugEnabled()) {
+			log.debug(group + ".ADDED: " + addResult);
+		}
+	}
+
+	private void modify(String group, Git git, StringBuilder msg, String pattern)
+			throws GitAPIException, NoFilepatternException {
+		AddCommand update = git.add().addFilepattern(pattern);
+		msg.append("Updated: " + pattern + "\n");
+		DirCache addResultUpdate = update.call();
+		if (log.isDebugEnabled()) {
+			log.debug(group + ".UPDATED: " + addResultUpdate);
+		}
+	}
+
+	private void delete(String group, Git git, StringBuilder msg, String pattern)
+			throws GitAPIException, NoFilepatternException {
+		RmCommand delete = git.rm().addFilepattern(pattern);
+		msg.append("Deleted: " + pattern + "\n");
+		DirCache deleteResult = delete.call();
+		if (log.isDebugEnabled()) {
+			log.debug(group + ".DELETED: " + deleteResult);
+		}
+	}
+
+	private void commitWrite(String group, StringBuilder msg, IGitProvider provider) throws GitAPIException {
+		String tmp = msg.toString();
+		if (log.isInfoEnabled()) {
+			log.info(group + ".MESSAGE:\n" + tmp);
+		}
+		RevCommit commit = provider.commitWrite(group, tmp);
+		if (log.isInfoEnabled()) {
+			log.info(group + ".COMMIT: " + commit);
 		}
 	}
 }
