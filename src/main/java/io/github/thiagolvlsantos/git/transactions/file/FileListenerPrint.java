00e0921bcdefaf7cdfbf0e667f767d93f0112988
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-06-07 21:12:42 -0300

    Sonar: remove sysout

--- src/main/java/io/github/thiagolvlsantos/git/transactions/file/FileListenerPrint.java
+++ src/main/java/io/github/thiagolvlsantos/git/transactions/file/FileListenerPrint.java
@@ -4,12 +4,17 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
 @Profile("test")
+@Slf4j
 public class FileListenerPrint implements ApplicationListener<FileEvent> {
 
 	@Override
 	public void onApplicationEvent(FileEvent event) {
-		System.out.println("ITEMS: " + event.getItems());
+		if (log.isInfoEnabled()) {
+			log.info("ITEMS: " + event.getItems());
+		}
 	}
 }
