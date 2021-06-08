5aeec35deb557b2cd046e23039a51339051036bf
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-06-07 21:03:26 -0300

    Sonar: Serializable fix.

--- src/main/java/io/github/thiagolvlsantos/git/transactions/file/FileItem.java
+++ src/main/java/io/github/thiagolvlsantos/git/transactions/file/FileItem.java
@@ -1,6 +1,7 @@
 package io.github.thiagolvlsantos.git.transactions.file;
 
 import java.io.File;
+import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +14,8 @@ import lombok.ToString;
 @NoArgsConstructor
 @Builder
 @ToString
-public class FileItem {
+@SuppressWarnings("serial")
+public class FileItem implements Serializable {
 
 	private File file;
 	private EFileStatus status;
