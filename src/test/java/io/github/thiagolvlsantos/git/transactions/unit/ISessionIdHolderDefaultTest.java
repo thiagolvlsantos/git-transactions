2631e6f74e017be2eda5dee48b4840d90b71494a
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-06-13 20:12:19 -0300

    Helper.

---
+++ src/test/java/io/github/thiagolvlsantos/git/transactions/unit/ISessionIdHolderDefaultTest.java
@@ -0,0 +1,26 @@
+package io.github.thiagolvlsantos.git.transactions.unit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+
+import io.github.thiagolvlsantos.git.transactions.id.ISessionIdHolder;
+import io.github.thiagolvlsantos.git.transactions.id.SessionIdHolderHelper;
+
+@ContextConfiguration(classes = ISessionIdHolderDefaultTest.Config.class)
+@SpringBootTest
+class ISessionIdHolderDefaultTest {
+
+	public static class Config {
+	}
+
+	@Test
+	void defined(@Autowired ApplicationContext ctx) throws Exception {
+		ISessionIdHolder s = SessionIdHolderHelper.holder(ctx);
+		assertThat(s.current()).isNotBlank();
+	}
+}
---
+++ src/test/java/io/github/thiagolvlsantos/git/transactions/unit/ISessionIdHolderTest.java
@@ -0,0 +1,37 @@
+package io.github.thiagolvlsantos.git.transactions.unit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+
+import io.github.thiagolvlsantos.git.transactions.id.ISessionIdHolder;
+import io.github.thiagolvlsantos.git.transactions.id.SessionIdHolderHelper;
+
+@ContextConfiguration(classes = ISessionIdHolderTest.Config.class)
+@SpringBootTest
+class ISessionIdHolderTest {
+
+	public static class Config {
+		@Bean
+		public ISessionIdHolder holder() {
+			return new ISessionIdHolder() {
+
+				@Override
+				public String current() {
+					return "fixed";
+				}
+			};
+		}
+	}
+
+	@Test
+	void defined(@Autowired ApplicationContext ctx) throws Exception {
+		ISessionIdHolder s = SessionIdHolderHelper.holder(ctx);
+		assertThat(s.current()).isEqualTo("fixed");
+	}
+}
