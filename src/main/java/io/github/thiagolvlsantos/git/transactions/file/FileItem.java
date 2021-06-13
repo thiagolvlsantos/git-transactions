bfdb2e1a1bd10dcdc49f7f789321bdfeae89c34d
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-06-13 20:27:29 -0300

    Remove unnecessary annotations.

--- src/main/java/io/github/thiagolvlsantos/git/transactions/file/FileItem.java
+++ src/main/java/io/github/thiagolvlsantos/git/transactions/file/FileItem.java
@@ -4,16 +4,10 @@ import java.io.File;
 import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@ToString
 @SuppressWarnings("serial")
 public class FileItem implements Serializable {
 
